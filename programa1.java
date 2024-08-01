6b3a7a674ba804acabf373776de35f1b3304a2f3
Jonathan Hernandez <jhernandez@lynks.com.co>
2024-08-01 09:54:16 -0500

    Se creo la función para sumar dos enteros

--- programa1.java
+++ programa1.java
@@ -2,5 +2,11 @@
 public class programa1{
     public static void main(String[] args){
         System.out.println("Hola Mundo desde APOII");
+        System.out.println("Introducción a GIT/GITHUB");
+    }
+
+    //Crea una funcion para sumar dos numeros enteros y retornar el resultado
+    public static int suma(int a, int b){
+        return a + b;
     }
 }
